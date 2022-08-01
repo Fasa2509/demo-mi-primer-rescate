cd7e465c5768b143f7ffb5a411c6afe8ff4b0191
Miguel Fasanella <miguellfasanellap@gmail.com>
2022-08-01 19:56:25 -0400

    mas fallos tontos

--- components/ui/NavbarIndex.tsx
+++ components/ui/NavbarIndex.tsx
@@ -32,10 +32,11 @@ const slugs = [
 export const NavbarIndex: FC = () => {
 
   const { scrolled, passedImage } = useContext( ScrollContext );
+  const router = useRouter();
 
   return (
     <>
-    <Header />
+    <Header index={ router.asPath === '/' } />
     {/* si ya se hace hecho scroll, le agrega la clase scrolled que hace que se reduzca su height */}
     {/* <nav className={ `${styles.navbar}${ scrolled ? ` ${ styles['navbar--scrolled'] }` : '' }${ !passedImage ? ` ${styles.navbar__index}` : '' }` }>
       
