19e0a9ee948f792a2fa68715e16fbad8c8fe70d0
Miguel Fasanella <miguellfasanellap@gmail.com>
2022-11-13 09:47:01 -0400

    ProductId solved

--- models/Sold.ts
+++ models/Sold.ts
@@ -1,11 +1,18 @@
 import mongoose from 'mongoose';
-const { model, Schema, Types } = mongoose;
+const { model, Schema } = mongoose;
 import {  } from '../interfaces';
 
 const soldSchema = new Schema({
-    productId: { type: Types.ObjectId, ref: 'Product' },
+    productId: { type: Schema.Types.ObjectId, ref: 'Product' },
     soldUnits: { type: Number, default: 0 },
+    updatedAt: { type: Number, default: () => Date.now() },
 });
+
+// soldSchema.pre('save', function( next ) {
+//     const sold = this;
+
+//     sold.updatedAt = (() => Date.now())();
+// })
 
 // @ts-ignore
 mongoose.models = {};
