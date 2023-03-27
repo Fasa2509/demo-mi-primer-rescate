04ac23e03f2a2ea01ee28d521e5ec645daffa368
Miguel Fasanella <miguellfasanellap@gmail.com>
2023-03-26 21:17:00 -0400

    Next@12.3.4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 100,
   swcMinify: true,
   webpack: ( config ) => {
     config.resolve.fallback = { fs: false, net: false, dns: false }
