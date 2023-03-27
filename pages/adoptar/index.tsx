f4ca3cc1ab325c97c83cf6da934d9ae09207df1a
Miguel Fasanella <miguellfasanellap@gmail.com>
2023-03-27 14:27:46 -0400

    Vercel no detecta anterior push

--- pages/adoptar/index.tsx
+++ pages/adoptar/index.tsx
@@ -10,6 +10,7 @@ const AdoptarPage: NextPage = () => {
     <MainLayout title={ 'Adopción' } H1={ 'Adopta' } pageDescription={ 'Proceso de adopción de mascotas en la fundación Mi Primer Rescate. Envia tu solicitud para adoptar cualquiera de nuestros animales rescatados y nos pondremos en contacto contigo a la brevedad posible.' } titleIcon={ <VolunteerActivism color='info' sx={{ fontSize: '1.5rem' }} /> } nextPage='/cambios' url='/adoptar'>
       
         <Typography>Página de adopción</Typography>
+        <Typography>¡Esta página todavía está en construcción!</Typography>
 
     </MainLayout>
   )
